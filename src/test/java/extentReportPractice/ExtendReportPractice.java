c5be4a8fb04d916403728a756fc20c90dfd7f801
ubaleashish6 <40279033+ubaleashish6@users.noreply.github.com>
2019-09-28 18:38:55 +0530

    Update ExtendReportPractice.java

--- src/test/java/extentReportPractice/ExtendReportPractice.java
+++ src/test/java/extentReportPractice/ExtendReportPractice.java
@@ -5,7 +5,9 @@ import org.testng.annotations.Test;
 public class ExtendReportPractice {
 	
 	public ExtendReportPractice(){
-		
+		aaaaa
+		bbbbbbb
+		cccccccccc	
 	}
 	@Test
 	public void generateExtentReports(){
