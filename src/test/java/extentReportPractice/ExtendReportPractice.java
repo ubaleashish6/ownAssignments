322c39091b59f054e7481fbb0985c77f9a880071
Ashish <43025039+ashishubale@users.noreply.github.com>
2019-09-28 18:13:04 +0530

    Update ExtendReportPractice.java
    added some code in constructor

--- src/test/java/extentReportPractice/ExtendReportPractice.java
+++ src/test/java/extentReportPractice/ExtendReportPractice.java
@@ -5,7 +5,9 @@ import org.testng.annotations.Test;
 public class ExtendReportPractice {
 	
 	public ExtendReportPractice(){
-		
+		test5;
+		test6;
+		test7;
 	}
 	@Test
 	public void generateExtentReports(){
