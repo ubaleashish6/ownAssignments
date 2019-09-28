f4d535d0b0a0834aca707491236323f1cb5a8c37
ubaleashish6 <ubaleashish6@gmail.com>
2019-09-28 10:56:01 +0530

    constructor and test method is added to extentReportPractice.java

--- src/test/java/extentReportPractice/ExtendReportPractice.java
+++ src/test/java/extentReportPractice/ExtendReportPractice.java
@@ -1,5 +1,15 @@
 package extentReportPractice;
 
+import org.testng.annotations.Test;
+
 public class ExtendReportPractice {
+	
+	public ExtendReportPractice(){
+		
+	}
+	@Test
+	public void generateExtentReports(){
+		
+	}
 
 }
