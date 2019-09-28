eb1bd0c6649c77916eb1fbdb65410aff9a960edc
ubaleashish6 <ubaleashish6@gmail.com>
2019-09-28 18:40:48 +0530

    few lines added in constructor

--- src/test/java/extentReportPractice/ExtendReportPractice.java
+++ src/test/java/extentReportPractice/ExtendReportPractice.java
@@ -5,17 +5,16 @@ import org.testng.annotations.Test;
 public class ExtendReportPractice {
 	
 	public ExtendReportPractice(){
-<<<<<<< HEAD
+
 		test10;
 		test11;
 		test12;
 		
-		
-=======
+
 		test1;
 		test2;
 		test3;
->>>>>>> da035337f5144d1a49f639fe80e2ff41bdde45a8
+
 	}
 	@Test
 	public void generateExtentReports(){
