b14fdd874563d537613b575d8692bd339004bd32
Ashish Ubale <ubaleashish6@gmail.com>
2020-08-04 11:34:51 +0530

    added more programs

--- src/test/java/extentReportPractice/ExtendReportPractice.java
+++ src/test/java/extentReportPractice/ExtendReportPractice.java
@@ -5,9 +5,7 @@ import org.testng.annotations.Test;
 public class ExtendReportPractice {
 	
 	public ExtendReportPractice(){
-		aaaaa
-		bbbbbbb
-		cccccccccc	
+	
 	}
 	@Test
 	public void generateExtentReports(){
